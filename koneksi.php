6d33607b23f0cc773ec308fdf73793a24bb65892
widamudrikah <mudrikahwida@gmail.com>
2023-10-11 11:08:21 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -8,6 +8,6 @@ $koneksi = mysqli_connect($host, $user, $pass, $db);
 if(mysqli_connect_errno()){
     echo "gagal terhubung ke server : " . mysqli_connect_error();
 }
-//echo "berhasil terhubung ke server !";
+echo "berhasil terhubung ke server !";
 
 ?>
